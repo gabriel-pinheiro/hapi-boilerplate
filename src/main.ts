fb414065f98cbcf3f995c5830cd7755e8d8e512a
Gabriel Pinheiro <gabrielctpinheiro@gmail.com>
2020-06-20 20:56:01 -0300

    Winston Logger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
 import { Server } from './server';
+import { ILogger, Logger, logger } from './utils/logger';
 
 async function bootstrap() {
     const container = new Container({
         autoBindInjectable: true,
     });
+
+    container.bind<ILogger>(Logger).toConstantValue(logger);
 
     container
         .resolve<Server>(Server)
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import { Provider } from "./utils/decorators/provider";
+import { ILogger, Logger } from './utils/logger';
+import { inject } from "inversify";
 
 @Provider()
 export class Server {
+    constructor(
+        @inject(Logger)
+        private readonly logger: ILogger,
+    ) { }
 
     start() {
         
