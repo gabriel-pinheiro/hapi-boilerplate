779bb30877853a95bbef4916203c0a2526454870
Gabriel Pinheiro <gabrielctpinheiro@gmail.com>
2020-06-20 21:46:08 -0300

    Adding Hapi

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ILogger, Logger, logger } from './utils/logger';
 import {configProvider, ConfigProvider} from "./config/config.service";
 
 async function bootstrap() {
+    logger.info('Resolving dependencies');
     const container = new Container({
         autoBindInjectable: true,
     });
@@ -12,9 +13,12 @@ async function bootstrap() {
     container.bind<ILogger>(Logger).toConstantValue(logger);
     container.bind<ConfigProvider>(ConfigProvider).toConstantValue(configProvider);
 
-    container
+    const server = await container
         .resolve<Server>(Server)
         .start();
+
+    process.on('SIGINT',  () => server.stop());
+    process.on('SIGTERM', () => server.stop());
 }
 
 bootstrap();
--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,35 @@ import { Provider } from "./utils/decorators/provider";
 import { ILogger, Logger } from './utils/logger';
 import { inject } from "inversify";
 import {ConfigProvider} from "./config/config.service";
+import * as Hapi from '@hapi/hapi';
 
 @Provider()
 export class Server {
+    private app: Hapi.Server;
+
     constructor(
         @inject(Logger)
         private readonly logger: ILogger,
         private readonly config: ConfigProvider
     ) { }
 
-    start() {
-        
+    async start(): Promise<Server> {
+        this.logger.info('Starting server');
+        this.app = new Hapi.Server({
+            host: this.config.get('SERVER_HOST'),
+            port: this.config.getNumber('SERVER_PORT')
+        });
+
+        await this.app.start();
+        this.logger.info('Server is running on ' + this.app.info.uri);
+
+        return this;
+    }
+
+    async stop() {
+        this.logger.info('Stopping server');
+        await this.app.stop();
+        this.logger.info('Bye :)');
+        process.exit(0);
     }
 }
